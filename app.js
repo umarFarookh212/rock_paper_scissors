f8db0969ad86dc0e4a896393661760f68e584f8d
umarfarookh <sdumarfarookh212@gmail.com>
2024-01-06 13:04:16 +0530

    used prompt for username in game and text color changing for win

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
-let userChoice=0;
-let compChoice=0;
+let userScore=0;
+let compScore=0;
 
+let userScorePoint= document.querySelector("#userScore");
+let compScorePoint= document.querySelector("#compScore");
+let userNameTest= window.prompt("Enter Your name");
+let userName= document.querySelector("#userName");
+
+userName.innerText= userNameTest;
 let choices= document.querySelectorAll(".choice");
+let msg= document.querySelector("#msg");
 
 choices.forEach((choice)=>{
   choice.addEventListener("click", ()=>{
@@ -16,7 +23,24 @@ const genCompChoice=()=>{
   return options[randIdx];
 }
 const matchDraw= ()=>{
-  console.log("draw");
+  msg.innerHTML= "Game Draw! Play again";
+  msg.style.backgroundColor= "rgb(12, 30, 65)";
+
+}
+const showWinner= (userWin,userChoice,compChoice)=>{
+  if(userWin==true){
+    msg.innerHTML= `${userNameTest} Win! Your ${userChoice} beats ${compChoice}`;
+    msg.style.backgroundColor= "green";
+    userScore++;
+    userScorePoint.innerText= userScore;
+  }
+  else{
+    msg.innerHTML= `Computer Win! ${compChoice} beats your ${userChoice}`;
+    msg.style.backgroundColor= "red";
+    compScore++;
+    compScorePoint.innerText= compScore;
+
+  }
 }
 const playGame= (userChoice)=>{
   console.log("User", userChoice);
@@ -28,16 +52,16 @@ const playGame= (userChoice)=>{
   }
   else{
     userWin= true;
-    if(userChoice==="rock"){
-      // paper scissor
-      userWin= compChoice==="paper"?false:true;
-    }
-    else if (userChoice === "scissor") {
-      // rock scissor
-      userWin = compChoice ==="rock"? true:false
+    if(userChoice === "rock"){
+      // paper, scissors
+      userWin= compChoice === "paper"? false:true
+    } else if (userChoice === "paper") {
+      // rock, scissors
+      userWin= compChoice === "rock"?true:false
     } else {
-      // rock paper
-      // userWin= compChoice === ""?
+      // userchoice= "scissor" remaining= paper, rock
+      userwin= compChoice ==="paper"?true:false
     }
-  }
+    showWinner(userWin,userChoice,compChoice);
+}
 }
